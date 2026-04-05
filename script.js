7ffc85d77d962ab1ec98278360d060772138e380
Ramashankar Yadav <161759035+RamashankarYadav@users.noreply.github.com>
2026-04-05 12:21:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-
-const student = {
-  name: "John"
-};
-
-
-function getKeys(obj) {
-  return Object.keys(obj);
-}
-
-
 console.log(getKeys(student)); 
+
+
+console.log(getKeys({ name: "John", age: 20, city: "Pune" }));
+
+console.log(getKeys({ name: "Ram" }));
